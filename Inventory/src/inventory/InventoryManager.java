d0d8b2cc9eb6c1b70b5d44543bd14adb177672a1
Klent Tangaro <39150565+yan2xme@users.noreply.github.com>
2026-04-17 21:51:33 +0800

    Added InventoryManager, InventorySystem, Product

--- Inventory/src/inventory/InventoryManager.java
+++ Inventory/src/inventory/InventoryManager.java
@@ -1,7 +1,48 @@
 package inventory;
 
+import java.util.Scanner;
+
 public class InventoryManager {
-    Product[] products = new Product[100];
 
 
+    Scanner console = new Scanner(System.in);
+
+    public void addProduct() {
+
+        System.out.println("Please enter your product:");
+        String productName = console.next();
+
+        System.out.println("Enter the price");
+        double price = console.nextDouble();
+
+        System.out.println("Enter the quantity");
+        int productQuantity = console.nextInt();
+
+    }
+
+    public void updateStock() {
+
+        System.out.println("Please enter your product:");
+        String productName = console.next();
+
+        System.out.println("Enter the price");
+        double price = console.nextDouble();
+
+        System.out.println("Enter the quantity");
+        int productQuantity = console.nextInt();
+
+    }
+
+    public void viewStock() {
+        System.out.println("Please enter your product:");
+        String productName = console.next();
+
+        System.out.println("Enter the price");
+        double price = console.nextDouble();
+
+        System.out.println("Enter the quantity");
+        int productQuantity = console.nextInt();
+
+
+    }
 }
--- Inventory/src/inventory/InventorySystem.java
+++ Inventory/src/inventory/InventorySystem.java
@@ -5,50 +5,8 @@ import java.util.Scanner;
 
 public class InventorySystem {
 
-    Scanner console = new Scanner(System.in);
+    Product[] products = new Product[100];
 
-    public void addProduct(){
-
-        Product[] products = new Product[100];
-
-        System.out.println("Please enter your product:");
-        String productName = console.next();
-
-        System.out.println("Enter the price");
-        double price = console.nextDouble();
-
-        System.out.println("Enter the quantity");
-        int productQuantity = console.nextInt();
 
     }
 
-    public void updateStock(){
-
-        Product[] products = new Product[100];
-
-        System.out.println("Please enter your product:");
-        String productName = console.next();
-
-        System.out.println("Enter the price");
-        double price = console.nextDouble();
-
-        System.out.println("Enter the quantity");
-        int productQuantity = console.nextInt();
-
-    }
-
-    public void viewStock(){
-
-        Product[] products = new Product[100];
-
-        System.out.println("Please enter your product:");
-        String productName = console.next();
-
-        System.out.println("Enter the price");
-        double price = console.nextDouble();
-
-        System.out.println("Enter the quantity");
-        int productQuantity = console.nextInt();
-
-    }
-}
