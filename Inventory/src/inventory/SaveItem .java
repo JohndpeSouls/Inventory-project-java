2b1a9db43de6ebb3ef53f64acaa074486b277ea2
RemuwelBalmoriJr <rebalmorijr@addu.edu.ph>
2026-04-19 16:50:51 +0800

    Update SaveItem .java

--- Inventory/src/inventory/SaveItem .java
+++ Inventory/src/inventory/SaveItem .java
@@ -1,6 +1,9 @@
 package inventory;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
 
 class SaveItem {
 
@@ -25,6 +28,26 @@ class SaveItem {
 		}
 		
 	}
+	public void loadData(String filePath, InventoryManager manager) {
+        int count = 0;
+
+    try (Scanner scanner = new Scanner(new File("src/inventory/stocks.txt"))) {
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(":");
+
+            String productName = parts[0];
+            double price = Double.parseDouble(parts[1]);
+            int productQuantity = Integer.parseInt(parts[2]);
+            manager.addProduct(productName,price,productQuantity);
+        }
+    }
+ 
+    catch (FileNotFoundException e) {
+    }
+
+    }
 	
 }
 
